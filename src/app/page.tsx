6d84f287cc417eda41745e606aa561a784d5f466
kfukuhar <kfukuhar@student.42tokyo.jp>
2025-04-03 19:19:03 +0900

    change .tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-export default function page() {
+import { NextPage } from 'next';
+
+const Page: NextPage = () => {
   return (
     <h1 className='text-red-500'>Hello World!</h1>
   )
 }
+
+export default Page;
