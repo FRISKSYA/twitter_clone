7b0fed969a27a7e1f848c2969ffbca587966f51b
kfukuhar <kfukuhar@student.42tokyo.jp>
2025-04-03 22:59:22 +0900

    add compornents #14

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
-import { NextPage } from 'next';
-
-const Page: NextPage = () => {
+export default function Page() {
   return (
-    <h1 className='text-red-500'>Hello World!</h1>
+    <h1 className='text-red-500'>Home Page</h1>
   )
 }
-
-export default Page;
