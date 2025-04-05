70f5101dbca038c58270056645ef3e6a44384b8c
kfukuhar <kfukuhar@student.42tokyo.jp>
2025-04-05 21:15:57 +0900

    add icons #14

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ WORKDIR /app
 
 COPY package*.json ./
 
+RUN npm install @react-icons/all-files
 RUN npm install
 
 COPY . .
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,15 @@
+import Link from 'next/link';
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { IoIosHome } from "react-icons/io";
+
 export default function Sidebar() {
     return (
-      <div>Sidebar</div>
+      <div>
+        <Link href='/'>
+          <FaSquareXTwitter className='w-16 h-16 cursor-pointer 
+          p-3 hover:bg-gray-100 rounded-full'>
+          </FaSquareXTwitter>
+        </Link>
+      </div>
     )
 }
