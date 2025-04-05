858c41ff5c5d6850943872e8b6c071f1eed5c6e3
kfukuhar <kfukuhar@student.42tokyo.jp>
2025-04-05 22:18:25 +0900

    small fix

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,7 +4,7 @@ import { IoIosHome } from "react-icons/io";
 
 export default function Sidebar() {
     return (
-      <div className='flex flex-col gap-4'>
+      <div className='flex flex-col gap-4 p-3'>
         <Link href='/'>
           <FaSquareXTwitter className='w-16 h-16 cursor-pointer 
           p-3 hover:bg-gray-100 rounded-full transition-all
@@ -20,7 +20,9 @@ export default function Sidebar() {
         </Link>
         <button className='bg-blue-400 text-white
         rounded-full hover:brightness-95 transition-all
-        duration-200 w-48 h-9 shadow-md hidden xl:inline'>
+        duration-200 w-48 h-9 shadow-md hidden xl:inline
+        font-semibold
+        '>
           Sign In
         </button>
       </div>
