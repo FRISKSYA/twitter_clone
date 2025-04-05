98b6772b3d74f19c7471bc973c507e2b591a5402
kfukuhar <kfukuhar@student.42tokyo.jp>
2025-04-05 21:55:22 +0900

    add Sidebar #14

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,12 +4,25 @@ import { IoIosHome } from "react-icons/io";
 
 export default function Sidebar() {
     return (
-      <div>
+      <div className='flex flex-col gap-4'>
         <Link href='/'>
           <FaSquareXTwitter className='w-16 h-16 cursor-pointer 
-          p-3 hover:bg-gray-100 rounded-full'>
+          p-3 hover:bg-gray-100 rounded-full transition-all
+          duration-200 '>
           </FaSquareXTwitter>
         </Link>
+        <Link href='/' className='flex items-center p-3
+        hover:bg-gray-100 rounded-full transition-all
+        duration-200 gap-2 w-fit
+        '>
+          <IoIosHome className='w-7 h-7'></IoIosHome>
+          <span className='font-bold hidden xl:inline'>Home</span>
+        </Link>
+        <button className='bg-blue-400 text-white
+        rounded-full hover:brightness-95 transition-all
+        duration-200 w-48 h-9 shadow-md hidden xl:inline'>
+          Sign In
+        </button>
       </div>
     )
 }
