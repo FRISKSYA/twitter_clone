0fb11bcaa940f23628c32f1fcb5e73d0388e0fde
kfukuhar <kfukuhar@student.42tokyo.jp>
2025-04-06 16:38:06 +0900

    add news title #16

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -1,7 +1,24 @@
-"use client";
+"use client"
+
+import { useEffect, useState } from 'react'
 
 export default function News() {
+    const [news, setNews] = useState([]);
+    const [articleNum, setArticleNum] = useState(3);
+    
+    useEffect(() => {
+      fetch('https://saurav.tech/NewsAPI/everything/cnn.json')
+      .then((res) => res.json())
+      .then((data) => {
+        setNews(data.articles);
+      });
+    }, []);
+    
     return (
-      <div>News</div>
-    )
+      <div>
+        {news.slice(0, articleNum).map((article) => (
+          <h1>{article.title}</h1>
+        ))}
+      </div>
+    );
 }
