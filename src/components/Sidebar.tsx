de2d2d51235e4b29f4fdee9fdc34fcc69a0f7ea1
kfukuhar <kfukuhar@student.42tokyo.jp>
2025-04-10 15:15:41 +0900

    wip

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,9 +7,12 @@ import { IoIosHome } from "react-icons/io";
 
 export default function Sidebar() {
       const { data: session } = useSession();
-      console.log(session);
+
+      console.log("Session state:", session);
     return (
       <div className='flex flex-col gap-4 p-3'>
+        <div>
+
         <Link href='/'>
           <FaSquareXTwitter className='w-16 h-16 cursor-pointer 
           p-3 hover:bg-gray-100 rounded-full transition-all
@@ -23,23 +26,40 @@ export default function Sidebar() {
           <span className='font-bold hidden xl:inline'>Home</span>
         </Link>
         {session ? (
-            <button className='bg-blue-400 text-white
-              rounded-full hover:brightness-95 transition-all
-              duration-200 w-48 h-9 shadow-md hidden xl:inline
-              font-semibold'
-              onClick={() => signOut()}
-            >
+          <button className='bg-blue-400 text-white
+          rounded-full hover:brightness-95 transition-all
+          duration-200 w-48 h-9 shadow-md hidden xl:inline
+          font-semibold'
+          onClick={() => signOut()}
+          >
               Sign Out
             </button>
           ) : (
             <button className='bg-blue-400 text-white
-              rounded-full hover:brightness-95 transition-all
-              duration-200 w-48 h-9 shadow-md hidden xl:inline
-              font-semibold'
-              onClick={() => signIn()}
+            rounded-full hover:brightness-95 transition-all
+            duration-200 w-48 h-9 shadow-md hidden xl:inline
+            font-semibold'
+            onClick={() => signIn()}
             >
               Sign In
             </button>
+          )
+        }
+        </div>
+        {
+          session && (
+            <div className='text-gray-700 text-sm flex items-center
+            cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-all
+            duration-200'>
+              <img 
+                src={session.user.image} 
+                alt="user-img" 
+                className='h-10 w-10 rounded-full xl:mr-2'/>
+              <div className='hidden xl:inline'>
+                <h4 className='font-bold'>{session.user.name}</h4>
+                <p className='text-gray-500'>@{session.user.username}</p>
+              </div>
+            </div>
           )
         }
       </div>
